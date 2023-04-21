5f6cb6315ae61d38833298e2141a48f1f1bd693f
Grace-k-wambua <gracewambua05@gmail.com>
2023-04-21 16:05:23 -0700

    I made it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-       	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
