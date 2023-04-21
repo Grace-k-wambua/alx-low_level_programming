ade0d59ffc1c4af0a28ea67c592163517726cb66
Grace-k-wambua <gracewambua05@gmail.com>
2023-04-21 04:51:47 -0700

    my program should compiled without warning

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line using printf
+ * Return: 0 (success)
+ */
+
+int main(void)
+}
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
